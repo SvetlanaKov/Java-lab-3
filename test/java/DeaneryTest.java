cb8c9f19feca9c3a48ce629f7f5edd5299d58564
SvetlanaKov <lanaiks2404@gmail.com>
2020-07-05 18:53:06 +0300

    test

--- test/java/DeaneryTest.java
+++ test/java/DeaneryTest.java
@@ -12,92 +12,75 @@ public class DeaneryTest {
         Group testG = new Group("new");
         testG.addStudent(testS);
         testG.addStudent(testS1);
-        testS.putMark(5);
-        testS1.putMark(3);
-        assertEquals(5,5);
-        assertEquals(3,3);
+        test.marksMaker();
+        for (Student s : test.studentsArr) {
+            assertNotNull(s.getMarks());
+            assertEquals(10, s.getMarks().size());
+        }
     }
 
-   /* @Test
-    public void headSelection() {
-        Deanery test1 = new Deanery();
-        Student testS2 = new Student(10, "Бондарчук Парфен Савелиевич");
-        Student testS3 = new Student(5, "Бессмертных Елена Лукьяновна");
-        Group testG1 = new Group("new");
-        testG1.addStudent(testS2);
-        testG1.addStudent(testS3);
-        testG1.setHead();
-        assertEquals(testS2,testS2);
-    }*/
-
-    @Test
+   @Test
     public void moveStudent() {
         Deanery test1 = new Deanery();
-        Student testS2 = new Student(10, "Бондарчук Парфен Савелиевич");
-        Student testS3 = new Student(5, "Бессмертных Елена Лукьяновна");
-        Group testG1 = new Group("new");
-        Group testG2 = new Group("new2");
-        testG1.addStudent(testS2);
-        testG1.addStudent(testS3);
-        test1.moveStudent(testS2,testG2);
-        assertEquals(testS2,testS2);
+       test1.JSONReadS("students.json");
+       test1.JSONReadG("groups.json");
+       test1.moveStudent(test1.findByIDInDeanery(10), test1.groupsArr.get(1));
+       for (Group g : test1.groupsArr){
+           assertEquals("Пуффендуй", test1.findByIDInDeanery(10).group.title);
+       }
     }
 
     @Test
     public void findByFIOInDeanery() {
         Deanery test2 = new Deanery();
         Student testS4 = new Student(10, "Бондарчук Парфен Савелиевич");
-        Group testG3 = new Group("new");
-        testG3.addStudent(testS4);
-        test2.findByFIOInDeanery("Бондарчук Парфен Савелиевич");
-        assertEquals(testS4,testS4);
+        Student testS1 = new Student(5, "Бессмертных Елена Лукьяновна");
+       test2.findByFIOInDeanery("Бондарчук Парфен Савелиевич");
+        for (Student s : test2.studentsArr) {
+            assertNotNull(s.getFio());
+            assertEquals("Бондарчук Парфен Савелиевич", test2.findByFIOInDeanery(testS4.getFio()));
+        }
     }
 
     @Test
     public void findByIDInDeanery() {
         Deanery test3 = new Deanery();
         Student testS5 = new Student(10, "Бондарчук Парфен Савелиевич");
-        Group testG4 = new Group("new");
-        testG4.addStudent(testS5);
-        test3.findByIDInDeanery(10);
-        assertEquals(testS5,testS5);
+        Student testS6 = new Student(5, "Бессмертных Елена Лукьяновна");
+        test3.findByIDInDeanery(5);
+        for (Student t : test3.studentsArr) {
+            assertNotNull(t.getId());
+            assertEquals(5, test3.findByIDInDeanery(testS6.getId()));
+        }
     }
 
-    @Test
+   @Test
     public void delitedByDean() {
         Deanery test4 = new Deanery();
         Student testS6 = new Student(10, "Бондарчук Парфен Савелиевич");
-        Group testG5 = new Group("new");
-        testG5.addStudent(testS6);
         test4.findByFIOInDeanery("Бондарчук Парфен Савелиевич");
+       testS6.putMark(2);
         test4.delitedByDean(10);
-        assertEquals(testS6,testS6);
+       assertEquals(true, testS6.medianaMark() < 2.5);
     }
 
     @Test
     public void groupStatistics() {
         Deanery test5 = new Deanery();
-        Student testS7 = new Student(10, "Бондарчук Парфен Савелиевич");
-        Student testS8 = new Student(5, "Бессмертных Елена Лукьяновна");
-        Group testG6 = new Group("new");
-        testG6.addStudent(testS7);
-        testG6.addStudent(testS8);
-        testS7.putMark(5);
-        testS8.putMark(3);
-        testG6.medianaGroupMark();
-        assertEquals(4.0D, 4.0D, 0.001D);
+        test5.JSONReadS("students.json");
+        test5.JSONReadG("groups.json");
+        for (Student u : test5.studentsArr){
+            assertNotNull(u.medianaMark());
+        }
     }
 
     @Test
     public void studentStatistics() {
         Deanery test6 = new Deanery();
-        Student testS9 = new Student(10, "Бондарчук Парфен Савелиевич");
-        Group testG7 = new Group("new");
-        testG7.addStudent(testS9);
-        testS9.putMark(5);
-        testS9.putMark(3);
-        testS9.medianaMark();
-        test6.studentStatistics();
-        assertEquals(4.0D,4.0D, 0.00D);
+        test6.JSONReadS("students.json");
+        test6.JSONReadG("groups.json");
+        for (Group p : test6.groupsArr){
+            assertNotNull(p.medianaGroupMark());
+        }
     }
 }
